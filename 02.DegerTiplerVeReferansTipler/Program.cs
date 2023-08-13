edd3fb980cfe29f4ef5cff0b8ec80e8e66d20596
gayemce <100995170+gayemce@users.noreply.github.com>
2023-08-13 12:58:16 +0300

    02.DegerTiplerVeReferansTipler

--- 02.DegerTiplerVeReferansTipler/Program.cs
+++ 02.DegerTiplerVeReferansTipler/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           //Değer tiplerin bellekte değeri tutulur
+            string name = "Gaye";
+            string name2 = name;
+            name2 = "Ahmet";
+
+            Console.WriteLine(name);
+            Console.WriteLine(name2); //değer tip örneği
+
+
+            //Referans tiplere bellekte referans değeri atanır
+            //Bir eşitleme yapıldığı zaman, referans tipler referens değerini değil numarasını alır
+            //Bir değişiklik yapıldığında o referans numarasına sahip kaç değişken varsa hepsi değişir
+            User user = new();
+            User user2 = user;
+            user2.Name = "Ahmet";
+
+            Console.WriteLine(user.Name);
+        }
+    }
+}
+
+class User
+{
+    public string Name = "Gaye";
+}
         }
     }
 }
