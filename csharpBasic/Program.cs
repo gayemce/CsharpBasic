7a9f315ba01de8c2938770ef5a243bac1421deea
= <=>
2023-08-13 12:54:30 +0300

    01.Degiskenler

--- csharpBasic/Program.cs
+++ csharpBasic/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //değişkenlere değer atanmadığında aldıkları ilk değer
+            string name; //null
+            int age; //0
+            bool isActive; //false
+
+            float n1 = 100;
+            int n2 = 200;
+            var n3 = n1 + n2;
+            Console.WriteLine(n3.GetType());
+
+            string nameLastName = "test test";
+
+            name = "deneme";
+
+            char karakter = 'A';
+
+            string aciklama = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+            Console.WriteLine(nameLastName);
+
+            Console.ReadLine();
+
         }
     }
 }
