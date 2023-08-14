136a4239f1272c1a6c83d33ab5758a560c511912
gayemce <100995170+gayemce@users.noreply.github.com>
2023-08-14 09:05:39 +0300

    08.Metotlar

--- 08.Metotlar/Program.cs
+++ 08.Metotlar/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x = 0;
+            int y = 2;
+
+            //Metot(x,y);
+            int r = Metot(x, y, 2);
+            Console.WriteLine($"Sonuc: {r}");
+        }
+
+        static void Metot()//geriye değer dönmeyen metot
+        {
+            var sonuc = 5 + 3;
+            Console.WriteLine($"Sonuç: {sonuc}");
+        }
+
+        static int Metot(int a, int b)//geriye değer dönen metot
+        {
+            int sonuc = a + b;
+            return sonuc;
+        }
+
+        static int Metot(int a, int b, int c)//geriye değer dönen metot
+        {
+            int sonuc = a * b + c;
+            return sonuc;
         }
     }
 }
